1fa3d9f3eb6ecee1e8b1a6d5aee31c25073d4511
noam-ackerman <noamac1812@gmail.com>
2021-07-27 18:42:57 +0200

    fixing display of latest update time to real local time display

--- appJS.js
+++ appJS.js
@@ -27,15 +27,19 @@ let day = days[now.getDay()];
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
 
-//latest city data update day and time based on location timezone
+//time based on location timezone
 function formatLocalTime(props) {
-  let localTime = new Date(props.dt * 1000);
-  let localTimeOffset = localTime.getTimezoneOffset() * 60;
-  localTime.setSeconds(
-    localTime.getSeconds() + localTimeOffset + props.timezone
-  );
-  let localHours = ("0" + localTime.getHours()).slice(-2);
-  let localMinutes = ("0" + localTime.getMinutes()).slice(-2);
+  //to display correct local time
+  let localTimeZone = props.timezone / 3600;
+  let unixTimestamp = Date.now() / 1000;
+  let time = new Date(unixTimestamp * 1000);
+  let localHours = (time.getUTCHours() + localTimeZone + 24) % 24;
+  let hours = ("0" + localHours).slice(-2);
+  let minutes = ("0" + time.getMinutes()).slice(-2);
+  //to display correct local day
+  let day = new Date(props.dt * 1000);
+  let dayOffset = day.getTimezoneOffset() * 60;
+  day.setSeconds(day.getSeconds() + dayOffset + props.timezone);
   let days = [
     "Sunday",
     "Monday",
@@ -45,9 +49,9 @@ function formatLocalTime(props) {
     "Friday",
     "Saturday",
   ];
-  let localDay = days[localTime.getDay()];
-
-  return `${localDay} ${localHours}:${localMinutes}`;
+  let localDay = days[day.getDay()];
+  //
+  return `${localDay} ${hours}:${minutes}`;
 }
 
 //display forecast
@@ -155,7 +159,7 @@ function displayWeather(response) {
   fahrenheitLink.innerHTML = `°F`;
   slash.innerHTML = `/`;
   let currentDate = document.querySelector("#date");
-  noteDate.innerHTML = "Latest data update in local timezone:";
+  noteDate.innerHTML = "Local time:";
   currentDate.innerHTML = formatLocalTime(response.data);
   weatherNote.innerHTML = response.data.weather[0].description;
   let windSpeed = Math.round(response.data.wind.speed);
